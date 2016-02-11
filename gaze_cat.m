2704676b78615c538d98147e6bd45515c5ac7829
Jan Freyberg <jan.freyberg@gmail.com>
2016-02-10 22:08:14 -0500

    Update script for trial length and number

--- gaze_cat.m
+++ gaze_cat.m
@@ -31,7 +31,7 @@ t_frames = round(f_screen / f_pictures);
 f_face = f_pictures/5; % this NEEDS to be an integer division of f_pictures
 
 % Trial Duration
-trialdur = 6;
+trialdur = 20;
 
 % Break Duration
 breakdur = 4;
@@ -53,8 +53,8 @@ nrep = 6;
 scr_background = 127.5;
 
 % Image Folders & Numbers
-folder_objects = fullfile(pwd, 'Stimuli', 'sideways');
-folder_faces = fullfile(pwd, 'Stimuli', 'frontal');
+folder_objects = fullfile(pwd, 'Stimuli', 'frontal');
+folder_faces = fullfile(pwd, 'Stimuli', 'sideways');
 
 filenames_objects = dir(fullfile(folder_objects, '*.jpg'));
 filenames_faces = dir(fullfile(folder_faces, '*.jpg'));
@@ -141,7 +141,7 @@ flicker_wave = (cos(linspace(pi, 3*pi, t_frames)) + 1)/2;
 instrucs = ['In the following few trials, we just want to measure your brain response to\n\n',...
             'natural images. We are going to present these images in quick succession for\n\n'...
             '60s. During this time, please keep your eyes fixated on the images, and stay\n\n'...
-            'alert. There will be X trials, and in between trials you will have some time\n\n'...
+            'alert. There will be 12 trials, and in between trials you will have some time\n\n'...
             'to rest your eyes. Press the Space Bar when you are ready to begin.'];
 DrawFormattedText(scr, instrucs, 'center', 'center', 0);
 Screen('Flip', scr);
@@ -149,71 +149,71 @@ KbStrokeWait;
 
 
 %% Demonstration
-% Display Configuring Screen
-DrawFormattedText(scr, 'Configuring...', 'center', 'center', 0);
-Screen('Flip', scr);
-
-% Load stimuli for this trial
-n_trialstims = trialdur * f_pictures;
-list_trialstims = randperm(n_objects, n_trialstims);
-for iStim = 1:numel(list_trialstims)
-    temp_image = imread( fullfile(folder_objects, filenames_objects(list_trialstims(iStim)).name) );
-    temp_alpha = ~(sum(temp_image, 3) >= 3*250) * 255;
-    temp_image = cat(3, temp_image, temp_alpha);
-    temp_image( ~any(temp_alpha, 2), :, : ) = []; % delete rows
-    temp_image( :, ~any(temp_alpha, 1), : ) = []; % delete cols
-    textures_trial{iStim} = Screen('MakeTexture', scr, temp_image);
-end
-
-% Replace every 5th with a face
-n_trialfaces = floor(trialdur * f_face);
-list_trialfaces = randperm(n_faces, n_trialfaces);
-for iFace = 1:n_trialfaces
-    Screen('Close', textures_trial{iFace * f_pictures/f_face});
-    temp_image = imread( fullfile(folder_faces, filenames_faces(list_trialfaces(iFace)).name) );
-    temp_alpha = ~(sum(temp_image, 3) >= 3*250) * 255;
-    temp_image = cat(3, temp_image, temp_alpha);
-    temp_image( ~any(temp_alpha, 2), :, : ) = []; % delete rows
-    temp_image( :, ~any(temp_alpha, 1), : ) = []; % delete cols
-    textures_trial{iFace * f_pictures/f_face} = Screen('MakeTexture', scr, temp_image);
-end
-
-% Pre-Trial
-DrawFormattedText(scr, ['Practice Trial', '\nReady?'], 'center', ycen-pxsize/3, 0);
-Screen('DrawLines', scr, fixLines, fixWidth, fixColor, [xcen, ycen]);
-Screen('Flip', scr);
-KbStrokeWait;
-Screen('DrawLines', scr, fixLines, fixWidth, fixColor, [xcen, ycen]); 
-Screen('Flip', scr);
-WaitSecs(2);
-
-% Trial
-Priority(1);
-trial_frames = ceil(trialdur * f_screen);
-t_last = GetSecs;
-for i = 1:trial_frames;
-    Screen('DrawTexture', scr, textures_trial{ ceil(i/t_frames) }, [], [], [], [], flicker_wave(mod(i-1, t_frames)+1));
-    t_last = Screen('Flip', scr, t_last+0.5*frame_dur); % specify deadline for timing issues?
-end
-Priority(0);
-Screen('Close');
-
-% Take a Break
-KbQueueStart;
-for lapsedTime = 0:breakdur
-DrawFormattedText(scr, ['Break for ' num2str(breakdur-lapsedTime)], 'center', ycen-pxsize/3, 0);
-Screen('Flip', scr);
-[~, pressed] = KbQueueCheck;
-if pressed(esc_key)
-    error('Interrupted in the break!');
-end
-WaitSecs(1);
-end
-KbQueueStop;
+% % Display Configuring Screen
+% DrawFormattedText(scr, 'Configuring...', 'center', 'center', 0);
+% Screen('Flip', scr);
+% 
+% % Load stimuli for this trial
+% n_trialstims = trialdur * f_pictures;
+% list_trialstims = randperm(n_objects, n_trialstims);
+% for iStim = 1:numel(list_trialstims)
+%     temp_image = imread( fullfile(folder_objects, filenames_objects(list_trialstims(iStim)).name) );
+%     temp_alpha = ~(sum(temp_image, 3) >= 3*250) * 255;
+%     temp_image = cat(3, temp_image, temp_alpha);
+%     temp_image( ~any(temp_alpha, 2), :, : ) = []; % delete rows
+%     temp_image( :, ~any(temp_alpha, 1), : ) = []; % delete cols
+%     textures_trial{iStim} = Screen('MakeTexture', scr, temp_image);
+% end
+% 
+% % Replace every 5th with a face
+% n_trialfaces = floor(trialdur * f_face);
+% list_trialfaces = randperm(n_faces, n_trialfaces);
+% for iFace = 1:n_trialfaces
+%     Screen('Close', textures_trial{iFace * f_pictures/f_face});
+%     temp_image = imread( fullfile(folder_faces, filenames_faces(list_trialfaces(iFace)).name) );
+%     temp_alpha = ~(sum(temp_image, 3) >= 3*250) * 255;
+%     temp_image = cat(3, temp_image, temp_alpha);
+%     temp_image( ~any(temp_alpha, 2), :, : ) = []; % delete rows
+%     temp_image( :, ~any(temp_alpha, 1), : ) = []; % delete cols
+%     textures_trial{iFace * f_pictures/f_face} = Screen('MakeTexture', scr, temp_image);
+% end
+% 
+% % Pre-Trial
+% DrawFormattedText(scr, ['Practice Trial', '\nReady?'], 'center', ycen-pxsize/3, 0);
+% Screen('DrawLines', scr, fixLines, fixWidth, fixColor, [xcen, ycen]);
+% Screen('Flip', scr);
+% KbStrokeWait;
+% Screen('DrawLines', scr, fixLines, fixWidth, fixColor, [xcen, ycen]); 
+% Screen('Flip', scr);
+% WaitSecs(2);
+% 
+% % Trial
+% Priority(1);
+% trial_frames = ceil(trialdur * f_screen);
+% t_last = GetSecs;
+% for i = 1:trial_frames;
+%     Screen('DrawTexture', scr, textures_trial{ ceil(i/t_frames) }, [], [], [], [], flicker_wave(mod(i-1, t_frames)+1));
+%     t_last = Screen('Flip', scr, t_last+0.5*frame_dur); % specify deadline for timing issues?
+% end
+% Priority(0);
+% Screen('Close');
+% 
+% % Take a Break
+% KbQueueStart;
+% for lapsedTime = 0:breakdur
+% DrawFormattedText(scr, ['Break for ' num2str(breakdur-lapsedTime)], 'center', ycen-pxsize/3, 0);
+% Screen('Flip', scr);
+% [~, pressed] = KbQueueCheck;
+% if pressed(esc_key)
+%     error('Interrupted in the break!');
+% end
+% WaitSecs(1);
+% end
+% KbQueueStop;
 
 
 %% Go through Trials
-for iTrial = 1:3
+for iTrial = 1:12
     
     % Display Configuring Screen
     DrawFormattedText(scr, 'Configuring...', 'center', 'center', 0);
@@ -228,6 +228,8 @@ for iTrial = 1:3
         temp_image = cat(3, temp_image, temp_alpha);
         temp_image( ~any(temp_alpha, 2), :, : ) = []; % delete rows
         temp_image( :, ~any(temp_alpha, 1), : ) = []; % delete cols
+        temp_image = imresize( temp_image, pxsize/size(temp_image, 2) ); % resize image to be 200px wide
+        
         textures_trial{iStim} = Screen('MakeTexture', scr, temp_image);
     end
 
@@ -241,6 +243,8 @@ for iTrial = 1:3
         temp_image = cat(3, temp_image, temp_alpha);
         temp_image( ~any(temp_alpha, 2), :, : ) = []; % delete rows
         temp_image( :, ~any(temp_alpha, 1), : ) = []; % delete cols
+        temp_image = imresize( temp_image, pxsize/size(temp_image, 2) ); % resize image to be 200px wide
+        
         textures_trial{iFace * f_pictures/f_face} = Screen('MakeTexture', scr, temp_image);
     end
 
